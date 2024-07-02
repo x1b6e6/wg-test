f2fba30be95ea1c4b10a81b6f46f8c86d81de3f9
Timofey Denisenko <ftdabcde@gmail.com>
2024-07-03 00:06:55 +0700

    print result in human readable format
    Signed-off-by: Timofey Denisenko <ftdabcde@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::time::UNIX_EPOCH;
+
 use libc::{NLM_F_ACK, NLM_F_DUMP, NLM_F_REQUEST};
 use netlink_packet_core::{
     NetlinkDeserializable, NetlinkMessage, NetlinkPayload, NetlinkSerializable,
@@ -6,7 +8,10 @@ use netlink_packet_generic::{
     ctrl::{nlas::GenlCtrlAttrs, GenlCtrl, GenlCtrlCmd},
     GenlFamily, GenlMessage,
 };
-use netlink_packet_wireguard::{nlas::WgDeviceAttrs, Wireguard, WireguardCmd};
+use netlink_packet_wireguard::{
+    nlas::{WgAllowedIp, WgAllowedIpAttrs, WgDeviceAttrs, WgPeerAttrs},
+    Wireguard, WireguardCmd,
+};
 use netlink_sys::{protocols::NETLINK_GENERIC, Socket, SocketAddr};
 
 fn socket_send<Message>(
@@ -73,6 +78,32 @@ where
     }
 }
 
+fn wg_allowed_ips(ips: Vec<WgAllowedIp>) -> Vec<String> {
+    ips.into_iter()
+        .filter_map(|ip| {
+            let mut found_ip = None;
+            let mut found_cidr = None;
+
+            for attr in ip.0.into_iter() {
+                match attr {
+                    WgAllowedIpAttrs::IpAddr(ip) => found_ip = Some(ip),
+                    WgAllowedIpAttrs::Cidr(cidr) => found_cidr = Some(cidr),
+                    _ => {}
+                }
+            }
+            let found_ip = found_ip?;
+            let found_cidr = found_cidr?;
+
+            Some(format!("{found_ip}/{found_cidr}"))
+        })
+        .collect()
+}
+
+fn wg_public_key(key: &[u8; 32]) -> String {
+    use base64::Engine as _;
+    base64::engine::general_purpose::STANDARD.encode(key)
+}
+
 #[derive(clap::Parser)]
 struct Args {
     interface: String,
@@ -127,7 +158,48 @@ async fn real_main() {
 
     let messages = socket_recv::<GenlMessage<Wireguard>>(&mut generic).unwrap();
 
-    log::info!("{messages:?}");
+    for msg in messages.into_iter() {
+        for nlas in msg.payload.nlas.into_iter() {
+            match nlas {
+                WgDeviceAttrs::IfName(iface) => println!("Interface: {iface}"),
+                WgDeviceAttrs::PrivateKey(_) => println!("Private key: (hidden)"),
+                WgDeviceAttrs::PublicKey(key) => println!("Public key: {}", wg_public_key(&key)),
+                WgDeviceAttrs::ListenPort(port) => println!("Listen port: {port}"),
+                WgDeviceAttrs::Fwmark(fwmark) if fwmark != 0 => println!("FwMark: {fwmark}"),
+                WgDeviceAttrs::Peers(peers) => peers.into_iter().for_each(|peer| {
+                    println!("\nPeer:");
+                    for nlas in peer.0.into_iter() {
+                        match nlas {
+                            WgPeerAttrs::PublicKey(key) => {
+                                println!("  Public key: {}", wg_public_key(&key))
+                            }
+                            WgPeerAttrs::PresharedKey(_) => println!("  Preshared key: (hidden)"),
+                            WgPeerAttrs::Endpoint(endpoint) => println!("  Endpoint: {endpoint}"),
+                            WgPeerAttrs::AllowedIps(ips) => {
+                                let ips = wg_allowed_ips(ips).join(", ");
+                                println!("  Allowed ips: {ips}")
+                            }
+                            WgPeerAttrs::PersistentKeepalive(keep_alive) if keep_alive != 0 => {
+                                println!("  KeepAlive: {keep_alive}")
+                            }
+                            WgPeerAttrs::LastHandshake(ts) if ts != UNIX_EPOCH => {
+                                match ts.elapsed() {
+                                    Ok(elapsed) => {
+                                        let elapsed = elapsed.as_secs();
+                                        println!("  Last handshake: {elapsed}s ago")
+                                    }
+                                    Err(err) => log::error!("{err}"),
+                                }
+                            }
+
+                            _ => {}
+                        }
+                    }
+                }),
+                _ => {}
+            }
+        }
+    }
 }
 
 fn main() {
